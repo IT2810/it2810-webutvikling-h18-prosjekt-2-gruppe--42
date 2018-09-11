18e34182e2b97b7ed07ee0d86806012a81b6225b
KMikalsen <KMikalsen@users.noreply.github.com>
2018-09-11 14:24:40 +0200

    Category/file information in App
    Issue #1

--- gruppe42/src/App.js
+++ gruppe42/src/App.js
@@ -1,15 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Media from './Components/Media/Media.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Media/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            sound: {
+                cat1: {
+                    file1: "Resources/Sound/buzz.mp3",
+                    file2: "Resources/Sound/cyberrace.mp3",
+                    file3: "Resources/Sound/desertshimmer.mp3",
+                    file1: "Resources/Sound/funmusic.mp3"
+                },
+                cat2: {
+                    file1: ""
+                },
+                cat3: {
+                    file1: ""
+                }
+            },
+            text: {
+                cat1: {
+                    file1: "Resources/Text/fifty-five.json",
+                    file2: "Resources/Text/fifty-four.json",
+                    file3: "Resources/Text/fifty-four2.json",
+                    file1: "Resources/Text/fifty.json"
+                },
+                cat2: {
+                    file1: ""
+                },
+                cat3: {
+                    file1: ""
+                }
+            }
+
+        }
+    }
+    render() {
+        return (<div className="App">
+            <Media/>
+            {/* <Categories/> */}
+        </div>);
+    }
 }
 
 export default App;
--- gruppe42/src/Components/Media/Media.js
+++ gruppe42/src/Components/Media/Media.js
@@ -5,19 +5,26 @@ import React, { Component } from 'react';
 class Media extends Component {
     constructor(props) {
         super(props)
-        this.state = {text: ""};
+        this.state = {text: "", image:""};
     }
     componentDidMount() {
         fetch("example.json").then(res => res.json() )
         .then(
             res => this.setState({'text':res.text})
         )
+
+        fetch("Resources/Images/example.svg").then(res => res.text() )
+        .then(
+            res => this.setState({image:res})
+        )
+
     }
 
   render() {
     return (
       <div className="Media">
         {this.state.text}
+        <svg dangerouslySetInnerHTML={{'__html':this.state.image}} />
       </div>
     );
   }
