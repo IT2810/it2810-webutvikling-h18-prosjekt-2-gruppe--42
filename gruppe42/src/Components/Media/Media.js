f428c8e5e652cda9a95465b679f6323bdec0489f
KMikalsen <KMikalsen@users.noreply.github.com>
2018-09-13 13:11:25 +0200

    Fixed audio player
    #10

--- gruppe42/src/Components/Media/Media.js
+++ gruppe42/src/Components/Media/Media.js
@@ -27,11 +27,11 @@ class Media extends Component {
 
     componentDidUpdate(prevProps, prevState) {
         let that = this
+        document.getElementById("player").pause()
         if(prevState.sound !== this.props.categories['sound'][this.props.soundCurrentCat][this.props.soundCurrentFile] ){
             this.setState({sound:this.props.categories['sound'][this.props.soundCurrentCat][this.props.soundCurrentFile]})
-            //document.getElementById("player").pause()
             document.getElementById("player").load()
-            document.getElementById("player").play()
+
         }
         if((prevProps.textCurrentCat !== this.props.textCurrentCat) || (prevProps.textCurrentFile !== this.props.textCurrentFile) ) {
             fetch(this.props.categories["text"][this.props.textCurrentCat][this.props.textCurrentFile]).then(res => res.json() )
@@ -52,6 +52,10 @@ class Media extends Component {
                 }
             }
             )
+
+            setTimeout(function () {
+                document.getElementById("player").play()
+            }, 150);
     }
 
   render() {
