ac73222408265e4519accaace1855dc08aae94fc
renatbec <renatebs94@gmail.com>
2018-09-13 13:18:11 +0200

    place category buttons on top of each other #7

--- gruppe42/src/Components/Media/Media.js
+++ gruppe42/src/Components/Media/Media.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './Media.css';
 
 class Media extends Component {
     constructor(props) {
@@ -67,7 +67,7 @@ class Media extends Component {
             <div className="media-grid-item text">
                 {this.state.text}
             </div>
-            
+            {audio}
         </div>
 
 
