219c280021ee733677fc8748496c3fd970a96ed8
KMikalsen <KMikalsen@users.noreply.github.com>
2018-09-14 15:09:59 +0200

    Changed tab design
    #12

--- gruppe42/src/Components/Files/Files.js
+++ gruppe42/src/Components/Files/Files.js
@@ -5,21 +5,26 @@ import './Files.css';
 class Files extends Component {
     constructor(props) {
         super(props)
-        this.state = {Compositions: ["Komp1", "Komp2", "komp3", "komp4"]};
+        this.state = {Compositions: ["Komp1", "Komp2", "komp3", "komp4"],
+                    active: 0};
         //console.log(this.props)
         this.onHandleChangeFile = this.onHandleChangeFile.bind(this)
     }
 
 	onHandleChangeFile(e) {
 		this.props.onChangeFile([this.props.type, e.target.value])
+        this.setState({active:e.target.value})
 	}
 
   render() {
     return (
       <div className="Files">
         {
-          Object.keys(this.state.Compositions).map((i, x) => 
-            <span classname="composition-button-span"><button className="composition-button" key={x} value={i} onClick={this.onHandleChangeFile}>Let me see composition #{i}</button></span>)
+          Object.keys(this.state.Compositions).map((i, x) =>
+            <span classname= "composition-button-span">
+                <button className={this.state.active == i ? "composition-button activeComp" : "composition-button" } key={x} value={i} onClick={this.onHandleChangeFile}>Let me see composition #{i}
+                </button>
+            </span>)
         }
       </div>
     );
