b2096c8cfc148fb0c8d2db86faff30f636fd7824
KMikalsen <KMikalsen@users.noreply.github.com>
2018-09-17 11:11:17 +0200

    Fixed active category highlight
    #6

--- gruppe42/src/Components/Files/Files.js
+++ gruppe42/src/Components/Files/Files.js
@@ -22,7 +22,7 @@ class Files extends Component {
         {
           Object.keys(this.state.Compositions).map((i, x) =>
             // <span classname= "composition-button-span">
-                <button className={this.state.active === i ? "composition-button activeComp" : "composition-button" } key={x} value={i} onClick={this.onHandleChangeFile}>Combo #{parseInt(i,10) + 1}
+                <button className={parseInt(this.state.active, 10) === parseInt(i, 10) ? "composition-button activeComp" : "composition-button" } key={x} value={i} onClick={this.onHandleChangeFile}>Combo #{parseInt(i,10) + 1}
                 </button>
             // </span>)
         )
